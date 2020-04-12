29e04b5ab189608bd33cb58fd9225eae389c03e0
max <592662285@qq.com>
2020-04-12 19:22:54 +0800

    通知记录表 dao

---
+++ src/main/kotlin/kavi/tech/service/mysql/entity/NoticeRecords.kt
@@ -0,0 +1,30 @@
+package kavi.tech.service.mysql.entity
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+import kavi.tech.service.mysql.component.AbstractEntity
+
+/**
+ * 分析结果数据表
+ * */
+@JsonNaming(PropertyNamingStrategy.LowerCaseStrategy::class)
+data class NoticeRecords(
+    var id: Long? = null,
+    var task_id: String? = null,                // 创建任务时的monoId
+    var mobile: String? = null,                 // 手机号码
+    var back_url: String? = null,             // 回调地址 数据推送地址
+    var result: String? = null,           // 单项数据结果
+    var carrier_001: String? = null,      // 预留1
+    var carrier_002: String? = null       // 预留2
+) : AbstractEntity() {
+
+    override fun tableName() = tableName
+
+    companion object {
+        /**
+         * 表名
+         * */
+        const val tableName = "carrier_notice_records"
+
+    }
+}
