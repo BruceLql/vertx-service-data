a09aa17d5920b2e34bc47f1d035fb1ad22dd8c01
litiezhu <804082784@qq.com>
2020-04-13 20:58:49 +0800

    fix

--- src/main/kotlin/kavi/tech/service/service/CallAnalysis.kt
+++ src/main/kotlin/kavi/tech/service/service/CallAnalysis.kt
@@ -48,6 +48,13 @@ class CallAnalysis {
                         var countLessThreeMonthTimeAvg: Int = countLessThreeMonthTime(mobile,task_id)/3 // 近3月主叫月均通话时长（秒）
                         var countLessSixMonthTimeAvg: Int = countLessSixMonthTime(mobile,task_id)/6 // 近6月主叫月均通话时长（秒）（秒）
 
+                        var becountLessOneMonth: Int = becountLessOneMonth(mobile,task_id) // 近1个月被叫通话次数
+                        var becountLessThreeMonth: Int = becountLessThreeMonth(mobile,task_id) // 近3个月被叫通话次数
+                        var becountLessSixMonth: Int = becountLessSixMonth(mobile,task_id) // 近6个月被叫通话次数
+                        var becountLessThreeMonthAvg: Int = becountLessThreeMonth(mobile,task_id)/3 // 近3月被叫月均通话次数
+                        var becountLessSixMonthAvg: Int = becountLessSixMonth(mobile,task_id)/6 // 近6个月被叫通话次数
+
+
 
 
 
@@ -60,7 +67,7 @@ class CallAnalysis {
 
     /***
      *
-     * 近1月通话次数
+     * 近1月主叫通话次数
      *
      * 近1月通话总次数（近1月是指近30天，即0-30天）
      */
@@ -85,7 +92,33 @@ class CallAnalysis {
     }
 
     /***
-     * 近3月通话次数
+     *
+     * 近1个月被叫通话次数
+     *
+     * 近1个月被叫通话次数（近1月是指近30天，即0-30天）
+     */
+    fun becountLessOneMonth(mobile: String,taskId: String ): Int {
+        var sql: String =
+            "SELECT\n" +
+                    "COUNT(id)\n" +
+                    "FROM\n" +
+                    "\tcarrier_voicecall\n" +
+                    "where \n" +
+                    " DATE(date_add(now(), interval -30 day))<\n" +
+                    "DATE(CONCAT(SUBSTR(bill_month,1,4),\"-\",time))\n" +
+                    "and  peer_number = \n" + mobile
+        "and task_id = \"" + taskId
+        var result: Int = 0
+        carrierResultDataDao.customizeSQL(sql).subscribe { list ->
+            var first: Int = list.size
+            result = first
+        }
+        return result
+
+    }
+
+    /***
+     * 近3月主叫通话次数
      *
      * 近3月通话总次数（近3月是指近三月的数据，即0-90天）
      */
@@ -110,7 +143,54 @@ class CallAnalysis {
     }
 
     /***
-     * 近6月通话总次数（近6月是指近六月的数据，即0-180天）
+     * 近3个月被叫通话次数
+     *
+     * 近3个月被叫通话次数（近3月是指近三月的数据，即0-90天）
+     */
+    fun becountLessThreeMonth(mobile: String,taskId: String ): Int {
+        var sql: String =
+            "SELECT\n" +
+                    "COUNT(id)\n" +
+                    "FROM\n" +
+                    "\tcarrier_voicecall\n" +
+                    "where \n" +
+                    " DATE(date_add(now(), interval -90 day))<\n" +
+                    "DATE(CONCAT(SUBSTR(bill_month,1,4),\"-\",time))\n" +
+                    "and  peer_number = \n" + mobile
+        "and task_id = \"" + taskId
+        var result: Int = 0
+        carrierResultDataDao.customizeSQL(sql).subscribe { list ->
+            var first: Int = list.size
+            result = first
+        }
+        return result
+
+    }
+
+    /***
+     * 近6月主叫月均通话次数（近6月是指近六月的数据，即0-180天）
+     */
+    fun becountLessSixMonth(mobile: String,taskId: String ): Int {
+        var sql: String =
+            "SELECT\n" +
+                    "COUNT(id)\n" +
+                    "FROM\n" +
+                    "\tcarrier_voicecall\n" +
+                    "where \n" +
+                    " DATE(date_add(now(), interval -180 day))<\n" +
+                    "DATE(CONCAT(SUBSTR(bill_month,1,4),\"-\",time))\n" +
+                    "and  peer_number = \n" + mobile
+        "and task_id = \"" + taskId
+        var result: Int = 0
+        carrierResultDataDao.customizeSQL(sql).subscribe { list ->
+            var first: Int = list.size
+            result = first
+        }
+        return result
+    }
+
+    /***
+     * 近6月主叫月均通话次数（近6月是指近六月的数据，即0-180天）
      */
     fun countLessSixMonth(mobile: String,taskId: String ): Int {
         var sql: String =
@@ -132,9 +212,8 @@ class CallAnalysis {
     }
 
 
-
     /**
-     *近1月通话时长（秒）
+     *近1月主叫通话时长（秒）
      */
     fun countLessOneMonthTime(mobile: String,taskId: String ): Int {
         var sql: String =
@@ -156,7 +235,7 @@ class CallAnalysis {
     }
 
     /**
-     *近3月通话时长（秒）
+     *近3月主叫通话时长（秒）
      */
     fun countLessThreeMonthTime(mobile: String,taskId: String ): Int {
         var sql: String =
@@ -178,7 +257,7 @@ class CallAnalysis {
     }
 
     /**
-     *近6月通话时长（秒）
+     *近6月主叫通话时长（秒）
      */
     fun countLessSixMonthTime(mobile: String,taskId: String ): Int {
         var sql: String =
