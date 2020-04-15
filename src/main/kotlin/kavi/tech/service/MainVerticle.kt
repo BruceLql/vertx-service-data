e5ddf4da4ed22d1e628c020d0ba9e3a1b05c79f6
changcaichao <ccc1179641965@163.com>
2020-04-15 20:26:33 +0800

    添加sha256加密工具类,优化用户通话分析、联系人区域分析部分代码

--- src/main/kotlin/kavi/tech/service/MainVerticle.kt
+++ src/main/kotlin/kavi/tech/service/MainVerticle.kt
@@ -1,5 +1,9 @@
 package kavi.tech.service
 
+import io.vertx.rxjava.ext.web.client.WebClient
+import kavi.tech.service.common.utils.DateUtils
+import kavi.tech.service.service.ContactsRegionService
+import kavi.tech.service.service.UserBehaviorService
 import kavi.tech.service.web.WebVerticle
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.ComponentScan
@@ -14,11 +18,27 @@ class MainVerticle : LauncherVerticle() {
 
     @Autowired
     private lateinit var webVerticle: WebVerticle
+    @Autowired
+    private lateinit var rxClient: WebClient
+
+    @Autowired
+    private lateinit var userBehaviorService: UserBehaviorService
+
+    @Autowired
+    private lateinit var  contactsRegionService: ContactsRegionService
 
     @Throws(Exception::class)
     override fun start() {
         super.start()
-        vertx.deployVerticle(webVerticle)
+//        vertx.deployVerticle(webVerticle)
+
+//        userBehaviorService.getCellBehavior("14779716260","5e944644ccb8428ff6b7dea6").subscribe {
+//            println(it)
+//        }
+
+        contactsRegionService.getContactRegion("14779716260","5e944644ccb8428ff6b7dea6").subscribe {
+            println(it)
+        }
     }
 
     companion object {
--- src/main/kotlin/kavi/tech/service/mysql/dao/ContactsRegionDao.kt
+++ src/main/kotlin/kavi/tech/service/mysql/dao/ContactsRegionDao.kt
@@ -30,12 +30,12 @@ class ContactsRegionDao @Autowired constructor(
      */
     fun getContactRegion(mobile: String, taskId: String): Single<List<JsonObject>> {
         //3月
-        val moth3 = DateUtils.getPreMothOrCurrentMoth(3L, DateUtils.Companion.DatePattern.YYYY_MM_DD.value)
-        val mothPair3 = DateUtils.getFirstOrLastDate(moth3.first, moth3.second, DateUtils.Companion.DatePattern.YYYY_MM_DD.value)
+        val moth3 = DateUtils.getPreMothOrCurrentMoth(3L, DateUtils.DatePattern.YYYY_MM_DD.value)
+        val mothPair3 = DateUtils.getFirstOrLastDate(moth3.first, moth3.second, DateUtils.DatePattern.YYYY_MM_DD.value)
 
         //6月
-        val moth6 = DateUtils.getPreMothOrCurrentMoth(6L, DateUtils.Companion.DatePattern.YYYY_MM_DD.value)
-        val mothPair6 = DateUtils.getFirstOrLastDate(moth6.first, moth6.second, DateUtils.Companion.DatePattern.YYYY_MM_DD.value)
+        val moth6 = DateUtils.getPreMothOrCurrentMoth(6L, DateUtils.DatePattern.YYYY_MM_DD.value)
+        val mothPair6 = DateUtils.getFirstOrLastDate(moth6.first, moth6.second, DateUtils.DatePattern.YYYY_MM_DD.value)
         return this.client.rxGetConnection().flatMap { conn ->
             Observable.concat(
                 listOf(
@@ -46,8 +46,8 @@ class ContactsRegionDao @Autowired constructor(
         }.map {
             (0..1).map { d ->
                 var json = JsonObject()
-                json.put("key", "contact_region_${if(d==0) 3 else 6}m")
-                json.put("desc", "联系人手机号码归属地 (近${if(d==0) 3 else 6}月联系次数降序)")
+                json.put("key", "contact_region_${if (d == 0) 3 else 6}m")
+                json.put("desc", "联系人手机号码归属地 (近${if (d == 0) 3 else 6}月联系次数降序)")
                 json.put("region_list", it[d])
             }
         }
@@ -79,7 +79,7 @@ class ContactsRegionDao @Autowired constructor(
                 "    (b.region_dialed_time / b.region_call_time) as region_dialed_time_pct\n" +
                 "from (\n" +
                 "         select\n" +
-                "             cv.homearea as region_loc,\n" +
+                "             if(length(cv.homearea)>0,cv.homearea,'不详') as region_loc,\n" +
                 "             count(distinct cv.peer_number) as region_uniq_num_cnt,\n" +
                 "             count(*) as region_call_cnt,\n" +
                 "             ifnull(sum(cv.duration_in_second),0) as region_call_time,\n" +
@@ -87,7 +87,7 @@ class ContactsRegionDao @Autowired constructor(
                 "             ifnull(sum(if(cv.dial_type='DIAL',cv.duration_in_second,0)),0) as region_dial_time,\n" +
                 "             count(if(cv.dial_type='DIALED',true,null)) as region_dialed_cnt,\n" +
                 "             ifnull(sum(if(cv.dial_type='DIALED',cv.duration_in_second,0)),0) as region_dialed_time\n" +
-                "         from carrier_voicecall cv where cv.mobile='$mobile' and cv.task_id='$taskId' and cv.time between '${mothPair.first}' and '${mothPair.second}' group by cv.homearea) as b order by b.region_call_cnt desc"
+                "         from carrier_voicecall cv where cv.mobile='$mobile' and cv.task_id='$taskId' and DATE(CONCAT(SUBSTR(cv.bill_month,1,4),'-',cv.time)) between '${mothPair.first}' and '${mothPair.second}' group by cv.homearea) as b order by b.region_call_cnt desc"
         return this.query(conn, sql)
     }
 }
