4f60e8d74474a6344c4ac1aabbfb959c42db8ec1
changcaichao <ccc1179641965@163.com>
2020-04-15 20:44:12 +0800

    冲突解决

--- src/main/kotlin/kavi/tech/service/MainVerticle.kt
+++ src/main/kotlin/kavi/tech/service/MainVerticle.kt
@@ -1,14 +1,12 @@
 package kavi.tech.service
 
-import io.vertx.rxjava.ext.web.client.WebClient
-import kavi.tech.service.service.ContactsRegionService
-import kavi.tech.service.service.UserBehaviorService
 import kavi.tech.service.web.WebVerticle
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Import
 import tech.kavi.vs.core.LauncherVerticle
 import tech.kavi.vs.web.HandlerRequestAnnotationBeanName
+
 
 @Import(BeanConfig::class)
 @ComponentScan(nameGenerator = HandlerRequestAnnotationBeanName::class)
@@ -17,14 +15,6 @@ class MainVerticle : LauncherVerticle() {
 
     @Autowired
     private lateinit var webVerticle: WebVerticle
-    @Autowired
-    private lateinit var rxClient: WebClient
-
-    @Autowired
-    private lateinit var userBehaviorService: UserBehaviorService
-
-    @Autowired
-    private lateinit var  contactsRegionService: ContactsRegionService
 
     @Throws(Exception::class)
     override fun start() {
