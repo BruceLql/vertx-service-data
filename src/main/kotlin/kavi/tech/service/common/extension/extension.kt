9aad3d7f7c2d1a3497d159880974a4c38ec069bd
max <592662285@qq.com>
2020-04-16 15:36:12 +0800

    手机正则匹配 增加

--- src/main/kotlin/kavi/tech/service/common/extension/extension.kt
+++ src/main/kotlin/kavi/tech/service/common/extension/extension.kt
@@ -7,7 +7,7 @@ import java.util.regex.Pattern
  * 手机号校验正则
  */
 fun regexPhone(phone: String): Boolean {
-    var mainRegex = "^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(18[0,1,2,3,5-9])|(177))\\d{8}$"
+    var mainRegex = "^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(16([0-9]))|(18[0-9])|(19[0-9])|(177))\\d{8}$"
     var p = Pattern.compile(mainRegex)
     val m = p.matcher(phone)
     return m.matches()
